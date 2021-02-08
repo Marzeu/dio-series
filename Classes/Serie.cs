76cec11befe7b3ce522e6be64bbe6a1786e34d05
Marzeu <marzahell@hotmail.com>
2021-02-08 15:37:27 -0300

    Classe Serie criada

---
+++ Classes/Serie.cs
@@ -0,0 +1,11 @@
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos 
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }                
+    }
+}
