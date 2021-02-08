c258773ee5ed1dc63363938cc119ee2e88a3782d
Marzeu <marzahell@hotmail.com>
2021-02-08 16:04:33 -0300

    interface de repositorio

--- Program.cs
+++ Program.cs
@@ -5,8 +5,7 @@ namespace DIO.Series
     class Program
     {
         static void Main(string[] args)
-        {
-            Serie meuObjeto = new Serie();
+        {           
 
             
             Console.WriteLine("Hello World!");
