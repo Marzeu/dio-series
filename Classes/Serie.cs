7297c5569b36c37844646287c9b9412ad1add376
Marzeu <marzahell@hotmail.com>
2021-02-08 15:53:28 -0300

    método ToString sobreescrito

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.Series
 {
     public class Serie : EntidadeBase
@@ -17,6 +19,18 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
-        }                
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano;
+            return retorno;
+        }
+
+        
     }
 }
