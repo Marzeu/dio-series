9bb3d4058b7555fde0bdf92465e0ea0593b61e0f
Marzeu <marzahell@hotmail.com>
2021-02-08 15:45:56 -0300

    metodo construtor

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -6,6 +6,17 @@ namespace DIO.Series
         private Genero Genero { get; set; }
         private string Titulo { get; set; }
         private string Descricao { get; set; }
-        private int Ano { get; set; }                
+        private int Ano { get; set; }
+
+        // Método construtor
+
+        public Serie (int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }                
     }
 }
